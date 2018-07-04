7c78fa5a811dd41327c42997a7ef1c98af37eae2
David Ladowitz <david@ladowitz.com>
2018-07-04 14:12:33 -0700

    finishing items request specs

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,32 @@ class ItemsController < ApplicationController
   end
 
   def show
-    # todo = Todo.find(params[:todo_id])
-    @item = Item.find(id: params[:id], todo_id: params[:todo_id])
-
+    @item = Item.where(todo_id: params[:todo_id]).find(params[:id])
     render json: @item, status: 200
+  end
+
+  def create
+    @item = Todo.find(params[:todo_id]).items.create!(item_params)
+    render json: @item, status: 201
+  end
+
+  def update
+    @item = Item.where(todo_id: params[:todo_id]).find(params[:id])
+    @item.update_attributes!(item_params)
+
+    head :no_content
+  end
+
+  def destroy
+    @item = Item.where(todo_id: params[:todo_id]).find(params[:id])
+    @item.delete
+
+    head :no_content
+  end
+
+  private
+
+  def item_params
+    params.permit(:name)
   end
 end
--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Todo Items API', type: :request do
   let(:id)      { items.first.id }
 
 
-  describe 'GET /todos/:todo_id/items' do
+  describe 'Index GET /todos/:todo_id/items' do
     before { get "/todos/#{todo_id}/items" }
 
     it "gets all the items" do
@@ -19,7 +19,7 @@ RSpec.describe 'Todo Items API', type: :request do
     end
   end
 
-  describe "GET /todos/:todo_id/items/1" do
+  describe "Show GET /todos/:todo_id/items/1" do
     context "when the item belongs to the todo" do
       before { get "/todos/#{todo_id}/items/1" }
 
@@ -44,6 +44,96 @@ RSpec.describe 'Todo Items API', type: :request do
 
       it "has a status code of 404" do
         expect(response.code).to eq "404"
+      end
+    end
+  end
+
+  describe "Create POST /todos/:todo_id/items" do
+    before { post "/todos/#{todo_id}/items", params: params }
+
+    context "with valid params" do
+      let(:params) { { name: "Practice API specs" } }
+
+      it "has a status code of 201" do
+        expect(response).to have_http_status 201
+      end
+
+      it "returns the created item" do
+        json = JSON.parse(response.body)
+        json["name"] = params[:name]
+      end
+    end
+
+    context "with invalid params" do
+      let(:params) { { name: nil } }
+
+      it "has a status status of 422" do
+        expect(response.status).to be 422
+      end
+
+      it "has an error message" do
+        json = JSON.parse(response.body)
+        expect(json["message"]).to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  describe "Update PUT /todos/:todo_id/items/1" do
+    before { put "/todos/#{todo_id}/items/#{id}", params: params }
+
+    context "with valid params" do
+      let(:params) { { name: "Practive SQL" } }
+
+      it "has a code of 204" do
+        expect(response.code).to eq "204"
+      end
+
+      it "updates the item name" do
+        expect(Item.find(id).name).to eq params[:name]
+      end
+
+      it "returns an empty body" do
+        expect(response.body).to be_empty
+      end
+    end
+
+    context "with invalid params" do
+      let(:params) { { name: nil } }
+
+      it "has a status of 422" do
+        expect(response.status).to eq 422
+      end
+
+      it "has an error message" do
+        expect(response.body
+
+        ).to match /Validation/
+      end
+    end
+  end
+
+  describe "Destroy DELETE /todos/:todo_id/items/1" do
+    before { delete "/todos/#{todo_id}/items/#{id}" }
+
+    context "with valid params" do
+      it "has status of 204" do
+        expect(response).to have_http_status 204
+      end
+
+      it "deletes the item" do
+        expect(Item.find_by(id: id)).to be_nil
+      end
+    end
+
+    context "with invalid params" do
+      let(:id) { 100 }
+
+      it "has status of 404" do
+        expect(response.code).to eq "404"
+      end
+
+      it "does not delete any items" do
+        expect(Item.count).to eq 20
       end
     end
   end
